d18414f8391ea2364c75fa52ce0daf766e3fcee1
shahGnp <gaurav.shah@sagarmatha.edu.np>
2023-01-02 01:46:18 +0545

    classifier complete

--- multinomial/scripts/classifier.py
+++ multinomial/scripts/classifier.py
@@ -9,3 +9,24 @@ from dataset_generator import get_topics
 import fitz
 import os
 
+import pickle
+
+nltk.download('stopwords')
+
+vectorizer=CountVectorizer()
+
+def NB_Model_Train(dataset):
+      return MultinomialNB().fit(vectorizer.fit_transform(dataset['Text']),dataset['Title'])
+
+def get_data_set():
+      df=pd.read_csv('Dataset_final.csv')
+      return df
+
+if __name__=='__main__':
+      print('Document Classifier')
+      dataset=get_data_set()
+      model = NB_Model_Train(dataset)
+      print('Model Training Successful !!')
+      #Saving model file somewhere to be used by another file.
+      pickle.dump(vectorizer, open('vectorizer.pickle', 'wb'))
+      pickle.dump(model,open('Classification.model','wb'))
